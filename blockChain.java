e48209cca0d3b857719787bde40537138ca4cf99
Vedant <vedantbhangale7@gmail.com>
2026-08-09 19:44:07 +0530

    Update blockChain.java

--- blockChain.java
+++ blockChain.java
@@ -1,5 +1,12 @@
 public class blockChain {
     public static void main(String[] args) {
-        
+        Block genesisBlock = new Block("Hi I am the first block", "0");
+        System.out.println("Hash for Block 1 : " + genesisBlock.hash);
+
+        Block secondBlock = new Block("Hi I am the second block", genesisBlock.hash);
+        System.out.println("Hash for Block 2 : " + secondBlock.hash);
+
+        Block thirdBlock = new Block("Hi I am the third block", secondBlock.hash);
+        System.out.println("Hash for Block 3 : " + thirdBlock.hash);
     }
 }
