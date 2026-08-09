55237224fa92c3623ada4bbfdb96a34b252bbfcb
Vedant <vedantbhangale7@gmail.com>
2026-08-09 22:27:12 +0530

    Added transaction functionality to main block class

--- Block.java
+++ Block.java
@@ -1,14 +1,15 @@
+import java.util.ArrayList;
 import java.util.Date;
 public class Block {
 
     public String hash;
     public String previousHash;
-    private String data;
+    public String merkleRoot;
+    public ArrayList<Transaction> transactions = new ArrayList<Transaction>();
     private long timeStamp;
-    private int nonce;
+    public int nonce;
 
-    public Block(String data, String previousHash) {
-        this.data = data;
+    public Block(String previousHash) {
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
         this.hash = calculateHash();
@@ -19,17 +20,32 @@ public class Block {
                 previousHash
                 + Long.toString(timeStamp)
                 + Integer.toString(nonce)
-                + data
+                + merkleRoot
         );
         return calculatedHash;
     }
 
     public void mineBlock(int difficulty) {
+        merkleRoot = StringUtil.getMerkleRoot(transactions);
         String target = new String(new char[difficulty]).replace('\0', '0');
         while(!hash.substring(0, difficulty).equals(target)){
             nonce++;
             hash = calculateHash();
         }
         System.out.println("Block Mined!! : " + hash);
+    }
+
+    public boolean addTransaction(Transaction transaction){
+        if(transaction == null) return false;
+        if((previousHash != "0")){
+            if((transaction.processTransaction() != true)){
+                System.out.println("Transaction failed to process. Discarded.");
+                return false;
+            }
+        }
+
+        transactions.add(transaction);
+        System.out.println("Transaction Successfully added to Block");
+        return true;
     }
 }
--- StringUtil.java
+++ StringUtil.java
@@ -1,5 +1,5 @@
 import java.security.*;
-import java.util.Base64;
+import java.util.*;
 
 public class StringUtil {
     public static String applySha256(String input){
@@ -49,5 +49,26 @@ public class StringUtil {
 
     public static String getStringFromKey(Key key){
         return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
+    public static String getMerkleRoot(ArrayList<Transaction> transactions){
+        int count = transactions.size();
+        ArrayList<String> previousTreeLayer = new ArrayList<String>();
+        for(Transaction transaction : transactions){
+            previousTreeLayer.add(transaction.transactionId);
+        }
+
+        ArrayList<String> treeLayer = new ArrayList<String>();
+        while(count > 1){
+            treeLayer = new ArrayList<String>();
+            for(int i = 1; i < previousTreeLayer.size(); i++){
+                treeLayer.add(applySha256(previousTreeLayer.get(i+1) + previousTreeLayer.get(i)));
+            }
+
+            count = treeLayer.size();
+            previousTreeLayer = treeLayer;
+        }
+        String merkleRoot = (treeLayer.size() == 1) ? treeLayer.get(0) : "";
+        return merkleRoot;
     }
 }
