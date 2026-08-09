d43ece4f2bc4e56785b47ebe9e6e39a22c5289e3
Vedant <vedantbhangale7@gmail.com>
2026-08-09 21:40:43 +0530

    Update Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -30,6 +30,16 @@ public class Transaction {
         );
     }
 
+    public void generateSignature(PrivateKey privateKey){
+        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(recipient) + Float.toString(value);
+        signature = StringUtil.applyECDSASig(privateKey, data);
+    }
+
+    public boolean verifySignature(){
+        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(recipient) + Float.toString(value);
+        return StringUtil.verifyECDSASig(sender, data, signature);
+    }
+
 }
 
 public class TransactionInput{
